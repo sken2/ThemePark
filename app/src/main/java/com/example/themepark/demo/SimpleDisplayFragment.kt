939045bcafeea5dfc24200945ed5a518db072dad
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-07-10 12:50:35 +0900

    demo app to refer theme

---
+++ app/src/main/java/com/example/themepark/demo/SimpleDisplayFragment.kt
@@ -0,0 +1,29 @@
+package com.example.themepark.demo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class SimpleDisplayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_simple_display, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button_pushme).apply {
+            setOnClickListener {
+                Toast.makeText(this@SimpleDisplayFragment.requireContext(), "You got it", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
